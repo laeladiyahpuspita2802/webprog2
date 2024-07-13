1172c95d3c0dcfd25de29717b2cd6c413255ec80
laeladiyahpuspita2802 <laeladiyahpuspita280203@gmail.com>
2024-07-13 22:15:44 +0700

    Update index.blade.php

--- resources/views/customer/package/index.blade.php
+++ resources/views/customer/package/index.blade.php
@@ -245,53 +245,46 @@ memiliki prioritas paling tinggi guna mempengaruhi hasil pada situasi tersebut.<
     <div class="container">
 
       <div class="section-title" data-aos="zoom-out">
-        <h2>Services</h2>
-        <p>What we do offer</p>
+      <h2>Batasan Aplikasi</h2>
+        <p>Ruang Lingkup Aplikasi</p>
       </div>
 
       <div class="row">
         <div class="col-lg-4 col-md-6">
           <div class="icon-box" data-aos="zoom-in-left">
             <div class="icon"><i class="las la-basketball-ball" style="color: #ff689b;"></i></div>
-            <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-            <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+            <h4 class="title"><a href="">Data Toko tidak terintegrasi</a></h4>
+            <p class="description"> Data yang digunakan adalah data toko yang rekomendasi di Jawa Tengah.</p>
           </div>
         </div>
         <div class="col-lg-4 col-md-6 mt-5 mt-md-0">
           <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="100">
             <div class="icon"><i class="las la-book" style="color: #e9bf06;"></i></div>
-            <h4 class="title"><a href="">Dolor Sitema</a></h4>
-            <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+            <h4 class="title"><a href="">Data Kriteria yang tidak dinamis</a></h4>
+            <p class="description">Data kriteria yang tersedia telah ditetapkan oleh pengembang aplikasi.</p>
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 mt-5 mt-lg-0 ">
           <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="200">
             <div class="icon"><i class="las la-file-alt" style="color: #3fcdc7;"></i></div>
-            <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-            <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            <h4 class="title"><a href="">Daftar Alternatif yang tidak dinamis</a></h4>
+            <p class="description">Alternatif yang tersedia untuk Sistem Pendukung Keputusan ini adalah beberapa daftar toko yang direkomendasikan.</p>
           </div>
         </div>
         <div class="col-lg-4 col-md-6 mt-5">
           <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="300">
             <div class="icon"><i class="las la-tachometer-alt" style="color:#41cf2e;"></i></div>
-            <h4 class="title"><a href="">Magni Dolores</a></h4>
-            <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <h4 class="title"><a href="">Metode Pendukung Keputusan</a></h4>
+            <p class="description">Sistem ini dirancang menggunakan metode Analytical Hierarchy Process.</p>
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 mt-5">
           <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="400">
             <div class="icon"><i class="las la-globe-americas" style="color: #d6ff22;"></i></div>
-            <h4 class="title"><a href="">Nemo Enim</a></h4>
-            <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6 mt-5">
-          <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="500">
-            <div class="icon"><i class="las la-clock" style="color: #4680ff;"></i></div>
-            <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
-            <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+            <h4 class="title"><a href="">Sistem terintegrasi dengan database</a></h4>
+            <p class="description"> Sistem berbasis website sehingga proses pengolahan data akan selalu real time, cepat, dan akurat.</p>
           </div>
         </div>
       </div>
@@ -377,138 +370,6 @@ memiliki prioritas paling tinggi guna mempengaruhi hasil pada situasi tersebut.<
 
     </div>
   </section><!-- End Portfolio Section -->
-
-  <!-- ======= Testimonials Section ======= -->
-  <section id="testimonials" class="testimonials">
-    <div class="container">
-
-      <div class="section-title" data-aos="zoom-out">
-        <h2>Testimonials</h2>
-        <p>What they are saying about us</p>
-      </div>
-
-      <div class="owl-carousel testimonials-carousel" data-aos="fade-up">
-
-        <div class="testimonial-item">
-          <p>
-            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-            Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
-            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-          </p>
-          <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
-          <h3>Saul Goodman</h3>
-          <h4>Ceo &amp; Founder</h4>
-        </div>
-
-        <div class="testimonial-item">
-          <p>
-            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-            Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-          </p>
-          <img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
-          <h3>Sara Wilsson</h3>
-          <h4>Designer</h4>
-        </div>
-
-        <div class="testimonial-item">
-          <p>
-            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-            Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
-            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-          </p>
-          <img src="assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
-          <h3>Jena Karlis</h3>
-          <h4>Store Owner</h4>
-        </div>
-
-        <div class="testimonial-item">
-          <p>
-            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-            Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
-            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-          </p>
-          <img src="assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
-          <h3>Matt Brandon</h3>
-          <h4>Freelancer</h4>
-        </div>
-
-        <div class="testimonial-item">
-          <p>
-            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-            Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-          </p>
-          <img src="assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
-          <h3>John Larson</h3>
-          <h4>Entrepreneur</h4>
-        </div>
-
-      </div>
-
-    </div>
-  </section><!-- End Testimonials Section -->
-
-  <!-- ======= F.A.Q Section ======= -->
-  <section id="faq" class="faq">
-    <div class="container">
-
-      <div class="section-title" data-aos="zoom-out">
-        <h2>Batasan Aplikasi</h2>
-        <p>Ruang Lingkup Aplikasi</p>
-      </div>
-
-      <ul class="faq-list">
-
-        <li data-aos="fade-up" data-aos-delay="100">
-          <a data-toggle="collapse" class="" href="#faq1">Data Toko tidak terintegrasi<i class="icofont-simple-up"></i></a>
-          <div id="faq1" class="collapse show" data-parent=".faq-list">
-            <p>
-              Data yang digunakan adalah data toko yang rekomendasi di Jawa Tengah.
-            </p>
-          </div>
-        </li>
-
-        <li data-aos="fade-up" data-aos-delay="200">
-          <a data-toggle="collapse" href="#faq2" class="collapsed">Data Kriteria yang tidak dinamis<i class="icofont-simple-up"></i></a>
-          <div id="faq2" class="collapse" data-parent=".faq-list">
-            <p>
-              Data kriteria yang tersedia telah ditetapkan oleh pengembang aplikasi.
-            </p>
-          </div>
-        </li>
-
-        <li data-aos="fade-up" data-aos-delay="300">
-          <a data-toggle="collapse" href="#faq3" class="collapsed">Daftar Alternatif yang tidak dinamis <i class="icofont-simple-up"></i></a>
-          <div id="faq3" class="collapse" data-parent=".faq-list">
-            <p>
-              Alternatif yang tersedia untuk Sistem Pendukung Keputusan ini adalah beberapa daftar toko yang direkomendasikan.
-            </p>
-          </div>
-        </li>
-
-        <li data-aos="fade-up" data-aos-delay="400">
-          <a data-toggle="collapse" href="#faq4" class="collapsed">Metode Pendukung Keputusan <i class="icofont-simple-up"></i></a>
-          <div id="faq4" class="collapse" data-parent=".faq-list">
-            <p>
-              Sistem ini dirancang menggunakan metode Analytical Hierarchy Process.
-            </p>
-          </div>
-        </li>
-
-        <li data-aos="fade-up" data-aos-delay="500">
-          <a data-toggle="collapse" href="#faq5" class="collapsed"> Sistem terintegrasi dengan database<i class="icofont-simple-up"></i></a>
-          <div id="faq5" class="collapse" data-parent=".faq-list">
-            <p>
-              Sistem berbasis website sehingga proses pengolahan data akan selalu real time, cepat, dan akurat.
-            </p>
-          </div>
-        </li>
-
-      </ul>
-
-    </div>
-  </section><!-- End F.A.Q Section -->
 
 </main><!-- End #main -->
 @endsection
