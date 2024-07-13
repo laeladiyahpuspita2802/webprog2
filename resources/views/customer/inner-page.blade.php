a0b1da930cb472b9c553ab38f6632648374b13d6
laeladiyahpuspita2802 <laeladiyahpuspita280203@gmail.com>
2024-07-13 22:20:47 +0700

    Update inner-page.blade.php

--- resources/views/customer/inner-page.blade.php
+++ resources/views/customer/inner-page.blade.php
@@ -104,9 +104,8 @@
   <!-- ======= Footer ======= -->
   <footer id="footer">
     <div class="container">
-      <h3>Selecao</h3>
-      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
-      <div class="social-links">
+      <h3>SPK Online Shop</h3>
+     <div class="social-links">
         <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
         <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
         <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
@@ -114,7 +113,7 @@
         <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
       </div>
       <div class="copyright">
-        &copy; Copyright <strong><span>Selecao</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>SPK Online Shop</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
